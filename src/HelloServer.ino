37a92a54b99a4d41d34b85335340b27bc004d1a3
Ralf Lauseker <ralf.lauseker@gmail.com>
2018-08-11 10:50:46 +0200

    Fixed display of number

--- src/HelloServer.ino
+++ src/HelloServer.ino
@@ -3,11 +3,21 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include "DHT.h"
+#include <PubSubClient.h>
+
+using namespace std;
 
 DHT dht;
 
 const char* ssid = "keller";
 const char* password = "1234321234321";
+const char* mqtt_server = "192.168.178.112";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
 ESP8266WebServer server(80);
 
@@ -41,31 +51,89 @@ void handleNotFound(){
   digitalWrite(led, 0);
 }
 
-void setup(void){
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
-  Serial.begin(115200);
+void setupWifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.println("");
 
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setupMDNS(void) {
   if (MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
+}
 
+void setupDHT(void) {
   dht.setup(2);
+}
 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP82-sensor-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("sensornode/1/ip", StringToChar(WiFi.localIP().toString()));
+      // ... and resubscribe
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+}
+
+void setupMQTT(void) {
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(mqttCallback);
+
+  client.subscribe("sensornode/1/+");
+}
+
+void setupWebServer(void) {
   server.on("/", handleRoot);
 
   server.on("/inline", [](){
@@ -78,6 +146,55 @@ void setup(void){
   Serial.println("HTTP server started");
 }
 
-void loop(void){
+void setup(void) {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  
+  setupWifi();
+  
+  setupMDNS();
+
+  setupDHT();
+
+  setupMQTT();
+
+  setupWebServer();
+}
+
+const char* StringToChar(String string) {
+  std::vector<char> writable(string.begin(), string.end());
+  writable.push_back('\0');
+
+  return &*writable.begin();
+}
+
+const char* floatToChar(float number) {
+  String string = String(number,2);
+  return StringToChar(string);
+} 
+
+void loop(void) {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 10000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 75, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("sensornode/1/log", msg);
+
+    float temperature = dht.getTemperature();
+    float humidity = dht.getHumidity();
+
+    client.publish("sensornode/1/temperature", floatToChar(temperature));
+    client.publish("sensornode/1/humidity", floatToChar(humidity));
+  }
+
   server.handleClient();
 }
